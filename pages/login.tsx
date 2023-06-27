21f61a7cb9e6b439cf4c5861f026ec0fba940637
Juliard Actub <56096375+codeMonkey20@users.noreply.github.com>
2023-06-27 16:09:26 +0800

    proceed button login queue

--- pages/login.tsx
+++ pages/login.tsx
@@ -178,7 +178,7 @@ export default function Login() {
                       );
                     }}
                   >
-                    Sign in form
+                    PROCEED
                   </Button>
                 </div>
 
